f889e1a22cec601297367231430b2c39857908cf
Aneurin Barker Snook <a@aneur.in>
2024-06-14 01:51:12 +0100

    exit 1 if publish fails

--- scripts/publish-gh-pages.js
+++ scripts/publish-gh-pages.js
@@ -1,7 +1,11 @@
 const ghpages = require('gh-pages')
+const process = require('process')
 
 ghpages.publish('dist', {
   cname: 'www.aneur.in',
 }, err => {
-  err && console.error(err)
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
 })
