c959b61f0197e1495708d1983925995e8c54b8b9
Aneurin Barker Snook <a@aneur.in>
2024-06-16 10:11:13 +0100

    add analytics

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import './style.scss'
 import Alpine from 'alpinejs'
+import Analytics from './components/Analytics'
 import Contact from './components/Contact'
 import Skills from './components/Skills'
 
 function main() {
   (window as unknown as { Alpine: typeof Alpine }).Alpine = Alpine
+  Alpine.data('analytics', Analytics(import.meta.env.VITE_ANALYTICS_WEBSITE_ID || ''))
   Alpine.data('contact', Contact)
   Alpine.data('skills', Skills)
   Alpine.start()
